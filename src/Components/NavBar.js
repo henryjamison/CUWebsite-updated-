4d11369ba16e3616de3421dc1c895cde5161e8e4
henryjamison <jamisonhc@appstate.edu>
2022-01-26 00:43:38 -0500

    Co-authored-by: mckylssirs <mckylssirs@users.noreply.github.com>

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,53 +1,73 @@
-import React , {useEffect} from 'react';
-import './NavBar.css';
-import { NavLink } from 'react-router-dom';
-import sigpi from '../sigpi.jpg';
+import React, { useEffect } from "react";
+import "./NavBar.css";
+import { NavLink } from "react-router-dom";
+
 
 const Navbar = () => {
+  const logo = (
+    <NavLink className="navbar-brand navbar-logo" to="/" exact>
+      <img
+        src="logo-trimmed-final.png"
+      ></img>
+    </NavLink>
+  );
 
-      return (
-        <nav className="navbar navbar-expand-lg navbar-dark navbar-mainbg">          
-          <div class="container-fluid">
-            <a class="navbar-brand" href="#">
-              <NavLink className="navbar-brand navbar-logo" to="/" exact>
-                <img src= "https://sigmapi.org/wp-content/uploads/2018/12/logomark-1-1024x541.png" width="300" height="140"></img>
+  return (
+    <nav
+      className="navbar navbar-expand-lg navbar-dark navbar-mainbg"
+      style={{ position: "fixed", top: 0, width: "100%", height: 80 }}
+    >
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          {logo}
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div
+          class="collapse navbar-collapse"
+          id="navbarNav"
+          style={{ textAlign: "right" }}
+        >
+          <ul class="navbar-nav">
+            <li className="nav-item active">
+              <NavLink className="nav-link" to="/" exact>
+                Home
               </NavLink>
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav" style={{textAlign: "right"}}>
-              <ul class="navbar-nav">
-                <li className="nav-item active">
-                  <NavLink className="nav-link" to="/" exact>
-                    Home
-                  </NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink className="nav-link" to="/about" exact>
-                    About
-                  </NavLink> 
-                </li>
-                <li className="nav-item">
-                  <NavLink className="nav-link" to="/alumini" exact>
-                    Alumini
-                  </NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink className="nav-link" to="/parents" exact>
-                    Parents
-                  </NavLink>
-                </li>
-                <li className="nav-item">
-                <NavLink className="nav-link" to="/rush" exact>
-                    Rush/Events
-                  </NavLink>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-      )
-  }
-  
-  export default Navbar;
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/about" exact>
+                About
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/alumni" exact>
+                Alumni
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/parents" exact>
+                Parents
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/rush" exact>
+                Rush/Events
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
